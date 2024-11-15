d60bd5adac967a074270b36a8538dc9a5597559f
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2024-11-15 15:28:43 +0600

    2.0

---
+++ assets/js/frontend.js
@@ -0,0 +1,129 @@
+// Define the configuration for Elementor frontend settings
+var elementorFrontendConfig = {
+    environmentMode: {
+        edit: false,
+        wpPreview: false,
+        isScriptDebug: false
+    },
+    i18n: {
+        shareOnFacebook: "Share on Facebook",
+        shareOnTwitter: "Share on Twitter",
+        pinIt: "Pin it",
+        download: "Download",
+        downloadImage: "Download image",
+        fullscreen: "Fullscreen",
+        zoom: "Zoom",
+        share: "Share",
+        playVideo: "Play Video",
+        previous: "Previous",
+        next: "Next",
+        close: "Close",
+        a11yCarouselWrapperAriaLabel: "Carousel | Horizontal scrolling: Arrow Left & Right",
+        a11yCarouselPrevSlideMessage: "Previous slide",
+        a11yCarouselNextSlideMessage: "Next slide",
+        a11yCarouselFirstSlideMessage: "This is the first slide",
+        a11yCarouselLastSlideMessage: "This is the last slide",
+        a11yCarouselPaginationBulletMessage: "Go to slide"
+    },
+    is_rtl: false,
+    breakpoints: {
+        xs: 0,
+        sm: 480,
+        md: 768,
+        lg: 1025,
+        xl: 1440,
+        xxl: 1600
+    },
+    responsive: {
+        breakpoints: {
+            mobile: {
+                label: "Mobile Portrait",
+                value: 767,
+                default_value: 767,
+                direction: "max",
+                is_enabled: true
+            },
+            mobile_extra: {
+                label: "Mobile Landscape",
+                value: 880,
+                default_value: 880,
+                direction: "max",
+                is_enabled: false
+            },
+            tablet: {
+                label: "Tablet Portrait",
+                value: 1024,
+                default_value: 1024,
+                direction: "max",
+                is_enabled: true
+            },
+            tablet_extra: {
+                label: "Tablet Landscape",
+                value: 1200,
+                default_value: 1200,
+                direction: "max",
+                is_enabled: false
+            },
+            laptop: {
+                label: "Laptop",
+                value: 1366,
+                default_value: 1366,
+                direction: "max",
+                is_enabled: false
+            },
+            widescreen: {
+                label: "Widescreen",
+                value: 2400,
+                default_value: 2400,
+                direction: "min",
+                is_enabled: false
+            }
+        },
+        hasCustomBreakpoints: false
+    },
+    version: "3.25.5",
+    is_static: false,
+    experimentalFeatures: {
+        additional_custom_breakpoints: true,
+        e_swiper_latest: true,
+        e_nested_atomic_repeaters: true,
+        e_optimized_control_loading: true,
+        e_onboarding: true,
+        e_css_smooth_scroll: true,
+        home_screen: true,
+        landing_pages: true,
+        nested_elements: true,
+        editor_v2: true,
+        link_in_bio: true,
+        floating_buttons: true
+    },
+    urls: {
+        assets: "https://concordgroupbd.com/wp-content/plugins/elementor/assets/",
+        ajaxurl: "https://concordgroupbd.com/wp-admin/admin-ajax.php",
+        uploadUrl: "https://concordgroupbd.com/wp-content/uploads"
+    },
+    nonces: {
+        floatingButtonsClickTracking: "e2acee668d"
+    },
+    swiperClass: "swiper",
+    settings: {
+        page: [],
+        editorPreferences: []
+    },
+    kit: {
+        active_breakpoints: ["viewport_mobile", "viewport_tablet"],
+        global_image_lightbox: "yes",
+        lightbox_enable_counter: "yes",
+        lightbox_enable_fullscreen: "yes",
+        lightbox_enable_zoom: "yes",
+        lightbox_enable_share: "yes",
+        lightbox_title_src: "title",
+        lightbox_description_src: "description"
+    },
+    post: {
+        id: 19,
+        title: "Concord%20Group%20-%20Leading%20Real%20Estate%20%26%20Construction%20Conglomerates",
+        excerpt: "",
+        featuredImage: "https://concordgroupbd.com/wp-content/uploads/2021/08/no-address-1024x576.jpeg"
+    }
+};
